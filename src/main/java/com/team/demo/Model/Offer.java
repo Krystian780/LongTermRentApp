44dcedcfc8cb36f0341a9cfc6cd2fd30d3f39352
Krystian MATYSEK (contractor) <krystian.matysek@amadeus.com>
2023-01-13 17:09:53 +0100

    sketch-ready

---
+++ src/main/java/com/team/demo/Model/Offer.java
@@ -0,0 +1,16 @@
+package com.team.demo.Model;
+
+import lombok.AllArgsConstructor;
+
+import java.io.File;
+
+@AllArgsConstructor
+public class Offer {
+    private String offerName;
+    private File file;
+    private Author author;
+
+
+
+
+}
