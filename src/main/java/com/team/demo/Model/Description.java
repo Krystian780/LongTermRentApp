b3eec50230df1256479d65c2e1f6f95e36a65c35
Krystian MATYSEK (contractor) <krystian.matysek@amadeus.com>
2023-01-13 17:02:23 +0100

    -h

---
+++ src/main/java/com/team/demo/Model/Description.java
@@ -0,0 +1,12 @@
+package com.team.demo.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Description {
+    private int price;
+    private String objectDescription;
+    private String amentities;
+}
