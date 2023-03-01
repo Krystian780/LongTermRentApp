6a294009ee4c3e824739e0def1a0b7308688f310
Krystian M <krystian2007@gmail.com>
2023-03-01 18:24:41 +0100

    test_has_been_corrected

--- src/test/java/com/team/demo/Service/OfferServiceTest.java
+++ src/test/java/com/team/demo/Service/OfferServiceTest.java
@@ -2,30 +2,38 @@ package com.team.demo.Service;
 
 import com.team.demo.OfferModel.City;
 import com.team.demo.OfferModel.Offer;
-import com.team.demo.Testing.TestingObjectCreator;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class OfferServiceTest {
 
+    @Autowired
+    OfferService offerService;
 
     @Test
     public void shouldReturnOfferForGivenCities(){
-        List<Offer> offers = new TestingObjectCreator().getOffers();
-        List<Offer> offersWithWarsawAsMainPlace = List.of(offers.get(2), offers.get(3));
-        List<String> citiesFromOffers = offersWithWarsawAsMainPlace.stream()
+        List<Offer> offersInWarsaw = offerService.getOffersForGivenCity("warszawa");
+        List<String> citiesFromOffers = offersInWarsaw.stream()
                 .map(Offer::getCity)
                 .map(City::getName)
                 .collect(Collectors.toList());
-        Set<String> allCititesFromGivenOffers = new HashSet<>(citiesFromOffers);
-        int size = allCititesFromGivenOffers.size();
-        assertEquals(1, size);
-        assertEquals(1,2);
+        Set<String> distinctCitiesNameFromObtainedOffer = new HashSet<>(citiesFromOffers);
+        int size = distinctCitiesNameFromObtainedOffer.size();
+        assertEquals(size, 1);
+        assertTrue(distinctCitiesNameFromObtainedOffer.contains("warszawa"));
     }
+
 }
